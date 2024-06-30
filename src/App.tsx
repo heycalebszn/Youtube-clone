fbb2bc2b07e4bfcc7796806f6fa11954f5336955
caleb <kalejaiyecaleb@gmail.com>
2024-06-30 02:39:15 +0100

    commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React , { useState } from 'react';
 import { PageHeader }  from './layouts/PageHeader';
 import { CategoryPills } from "./components/CategoryPills";
 import { categories } from "./data/Home";
@@ -10,12 +10,14 @@ const App = () => {
       <PageHeader />
       <div className="grid grid-cols-[auto,1fr] flex-grow-1 overflow-auto">
         <div>Sidebar</div>
+        <div className="over-x-hidden px-8 pb-4">
         <div className="sticky top-0 bg-white z-10 pb-4">
           <CategoryPills categories={categories}
           selectedCategory={selectedCategory} onSelect={setSelectedCategory} />
         </div>
       </div>
     </div>
+  </div>
   )
 }
 
--- src/components/CategoryPills.tsx
+++ src/components/CategoryPills.tsx
@@ -1,4 +1,6 @@
+import { ChevronLeft, ChevronRight } from "lucide-react";
 import { Button } from "./Button";
+import { useState, useRef} from "react";
 
 type CategoryPillProps = {
     categories: string[]
@@ -6,15 +8,62 @@ type CategoryPillProps = {
     onSelect: (category: string) => void
 }
 
+const TRANSLATE_AMOUNT = 200
+
 export function CategoryPills({ categories, selectedCategory, onSelect }: CategoryPillProps) {
+    const [isLeftVisible, setIsLeftVisible] = useState(false);
+    const [isRightVisible, setIsRightVisible] = useState(false);
+    const [translate, setTranslate] = useState(0);
+    const containerRef = useRef<HTMLDivElement>(null);
+
     return (
         <div className="overflow-x-hidden relative">
-            <div className="flex whitespace-nowrap gap-3 transition-transform w-[max-content]">
+            <div className="flex whitespace-nowrap gap-3 transition-transform w-[max-content]" style={{transform: `translateX(-${translate}px)`}}>
                 {categories.map(category => (
-                    <Button key={category} variant="dark" className="py-1 px-3 rounded-lg whitespace-nowrap">{category}</Button>
+                    <Button key={category}
+                    onClick={() => onSelect(category)}
+                    variant={selectedCategory === category ? "dark" : "default"} className="py-1 px-3 rounded-lg whitespace-nowrap">{category}</Button>
                 ))}
-                <Button className="py-1 px-3 rounded-lg whitespace-nowrap">JavaScript</Button>
             </div>
+            {isLeftVisible && (
+            <div className="absolute left-0 top-1/2 -translate-y-1/2 bg-gradient-to-r from-white from-50% to-transparent w-24 h-full">
+            <Button variant="ghost" onClick={() => {
+                setTranslate(translate => {
+                    const newTranslate = translate - TRANSLATE_AMOUNT;
+                    if(newTranslate <= 0) return 0
+                    return newTranslate
+                })
+            }} size="icon" className="h-full aspect-square w-auto p-1.5">
+            <ChevronLeft />
+            </Button>
+            </div>
+            )
+             }
+
+            {isRightVisible && (
+            <div className="absolute right-0 top-1/2 -translate-y-1/2 bg-gradient-to-l from-white from-50% to-transparent w-24 h-full justify-end">
+            <Button 
+            variant="ghost" 
+            size="icon" 
+            className="h-full aspect-square w-auto p-1.5"
+            onClick={() => {
+                setTranslate(translate => {
+                    if(containerRef.current == null) {
+                        return translate
+                    }
+                        const newTranslate = translate + TRANSLATE_AMOUNT;
+                        const edge = containerRef.current.scrollWidth
+                        const width = containerRef.current.clientWidth
+                        if(newTranslate + width >= edge) {
+                            return edge - width
+                        }
+                        return newTranslate
+                    })
+                }}>
+            <ChevronRight />
+            </Button>
+            </div>
+            )}
         </div>
     )
 }
